87950dba6d16ab01fd7679e8c698db774fa35370
unknown <ivantambunan122@gmail.com>
2023-04-20 12:06:12 +0700

    Tugas 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this -> call(IvanTable::class);
-
+        $this -> call(PostTableSeeder::class);
     }
 }
